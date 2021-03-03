2b9988d6263dca49a9c65d4fbd61d11225f0ca60
Nurbek Aidarken <nurbek0144@gmail.com>
2021-03-03 08:58:12 +0600

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,41 @@
 void main() {
   //Task1
-  // longestLine();
+//   longestLine();
 
   //Task2
-  var list = [60, 999, 14, "dart1", 45, 95, "dart", 1];
-  var value = "dart";
-  var value1 = 15;
+//   var list = [60, 999, 14, "dart1", 45, 95, "dart", 1];
+//   var value = "dart";
+//   var value1 = 15;
 
-  print(contain(list, value));
-  print(contain(list, value1));
+//   print(contain(list, value));
+//   print(contain(list, value1));
 
+//  Task3
+  var list = ["dart", "abc", "good luck"];
+  print(calculate(list));
 
+}
 
+List calculate(List<String> list){
+  List<int> valueList = new List();
+  var newList = new List();
+  int sum = 0;
+
+  for(String str in list){
+    newList.add(str.replaceAll(' ', '').toLowerCase());
+  }
+
+  for(String element in newList){
+      for(int index = 0; index < element.length; index++){
+          sum += element[index].codeUnitAt(0) - 96;
+      }
+      valueList.add(sum * (newList.indexOf(element) + 1));
+      sum = 0;
+  }
+
+  print(newList);
+
+  return valueList;
 }
 
 bool contain(List list, var value){
